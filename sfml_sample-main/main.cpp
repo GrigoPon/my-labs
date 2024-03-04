aed64011a74b247e9167383f0226024e28819e56
GrigoPon <ponyaev.grisha@gmail.com>
2024-03-05 00:30:07 +0200

    updated labwork

---
+++ sfml_sample-main/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <SFML/Graphics.hpp>
+#include <Circle.hpp>
+#include <Game.hpp>
+
+int main()
+{
+    srand(time(0));
+    gp::Game game(1280, 760, "LABA3");
+    game.Setup();
+    game.GAME();
+
+    return 0;
+}
